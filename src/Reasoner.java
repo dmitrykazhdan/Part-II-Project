f3531fbfd823ec3cca90974da106c7150cd7eaba
dmitrykazhdan <dmitry@kazhdan.net>
2017-11-08 20:45:20 +0000

    Setup owlapi and setup dependencies
    add owlapi.jar and Maven dependencies

--- src/Reasoner.java
+++ src/Reasoner.java
@@ -1,8 +1,20 @@
 
+import java.io.File;
+
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.*;
+
+
+
+
 public class Reasoner {
 	
-	public static void main(String args[]) {
+	public static void main(String args[]) throws OWLOntologyCreationException  {
 		
-		System.out.println("This environment is properly setup");
+		File file = new File("/Users/AdminDK/Desktop/TestOntology/pizza.owl");
+		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+		OWLOntology ontology = manager.loadOntologyFromOntologyDocument(file);
+		
+		System.out.println(ontology.toString());
 	}
 }
