81e61e44f8f740e39019d073c47b9c260703082d
dmitrykazhdan <dmitry@kazhdan.net>
2017-11-12 17:46:49 +0000

    Add Horridge's Explanation API

--- src/ExtractEntailments.java
+++ src/ExtractEntailments.java
@@ -1,13 +1,19 @@
-
 import java.io.File;
+import java.util.Set;
 
 import org.semanticweb.HermiT.Reasoner;
+import org.semanticweb.owl.explanation.api.Explanation;
+import org.semanticweb.owl.explanation.api.ExplanationGenerator;
+import org.semanticweb.owl.explanation.api.ExplanationGeneratorFactory;
+import org.semanticweb.owl.explanation.api.ExplanationManager;
 import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
-
+import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
+import org.semanticweb.owlapi.reasoner.structural.StructuralReasonerFactory;
 
 
 
@@ -19,13 +25,22 @@ public class ExtractEntailments {
 		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
 		OWLOntology ontology = manager.loadOntologyFromOntologyDocument(file);
 		
-		
-		OWLReasoner reasoner = new Reasoner(ontology);
+		// Usage of the reasoner:
+		OWLReasonerFactory reasonerFactory = new Reasoner.ReasonerFactory();
+		OWLReasoner reasoner = reasonerFactory.createReasoner(ontology);
 			
 		reasoner.precomputeInferences();
+			
 		System.out.println(reasoner.getUnsatisfiableClasses());
         System.out.println("Done");
 		
+
+        // Usage of the explanation API:
+		ExplanationGeneratorFactory<OWLAxiom> genFac = ExplanationManager.createExplanationGeneratorFactory(reasonerFactory);
+		ExplanationGenerator<OWLAxiom> gen = genFac.createExplanationGenerator(ontology);
+		OWLAxiom entailment = null;
+		Set<Explanation<OWLAxiom>> expl = gen.getExplanations(entailment, 5);
+
 		
 	}
 }
