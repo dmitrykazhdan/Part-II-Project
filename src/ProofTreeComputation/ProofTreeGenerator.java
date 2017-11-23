cd17855813b02093e1338e18074c5b61d4597774
dmitrykazhdan <dmitry@kazhdan.net>
2017-11-23 12:49:01 +0000

    Write Stubs for ProofTree Generator

--- src/ProofTreeComputation/ProofTreeGenerator.java
+++ src/ProofTreeComputation/ProofTreeGenerator.java
@@ -1,18 +1,47 @@
 package ProofTreeComputation;
 
+import java.util.List;
+
 import org.semanticweb.owl.explanation.api.Explanation;
 import org.semanticweb.owlapi.model.OWLAxiom;
 
 public class ProofTreeGenerator {
 
-	
-	private void ComputeProofTrees(Explanation<OWLAxiom> justification, OWLAxiom entailment) {
+	private List<ProofTree> ComputeInitialProofTrees(Explanation<OWLAxiom> justification, OWLAxiom entailment) {
 		
-		
-		
+		return null;		
 	}
 	
 	
+	private List<ProofTree> ComputeCompleteProofTrees(ProofTree initialTree) {
+		return null;
+	}
+	
+
+	private List<ProofTree> ComputeProofTrees(Explanation<OWLAxiom> justification, OWLAxiom entailment) {
+		
+		
+		// Think of better variable names
+		List<ProofTree> initialProofTreeList = ComputeInitialProofTrees(justification, entailment);
+		List<ProofTree> completeProofTreeList = null;
+		List<ProofTree> completeProofTrees = null;
+		
+		
+		for (ProofTree initialTree : initialProofTreeList) {
+			
+			
+			completeProofTrees = ComputeCompleteProofTrees(initialTree);
+			completeProofTreeList.addAll(completeProofTrees);
+			
+			
+		}
+		
+		return completeProofTreeList;		
+	}
+	
+	
+
+
 	public static ProofTree GenerateProofTree(OWLAxiom entailment, Explanation<OWLAxiom> justification) {
 		
 		/* Algorithm implementation goes here: */
