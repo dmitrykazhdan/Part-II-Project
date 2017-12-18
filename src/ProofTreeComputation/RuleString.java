771537f941d4045537f1187d687d4baf87604d07
dmitrykazhdan <dmitry@kazhdan.net>
2017-12-18 23:09:26 +0200

    Add description

---
+++ src/ProofTreeComputation/RuleString.java
@@ -0,0 +1,11 @@
+package ProofTreeComputation;
+
+public class RuleString {
+
+	// Note that the axiom consists of a constructor
+	// which links one or more class expressions together.
+	
+	
+	// Class expressions have their own constructors.
+	
+}
