84e1eda9f7c879640879066f4ac6c947ea5c8cf9
dmitrykazhdan <dmitry@kazhdan.net>
2018-01-31 00:03:02 +0000

    Add copying of failed files

--- src/CoverageAnalysis/GenerateTrees.java
+++ src/CoverageAnalysis/GenerateTrees.java
@@ -2,9 +2,13 @@ package CoverageAnalysis;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
@@ -27,8 +31,10 @@ public class GenerateTrees {
 	
 	public static void main(String args[]) throws IOException, InterruptedException, ExecutionException {
 
-		String explanationDirName = "/Users/AdminDK/Desktop/Explanations/";
-		File explanationDir = new File(explanationDirName);
+		
+		Path failedExplanationDirPath = Paths.get("/Users/AdminDK/Desktop/FailedExplanations/");
+		Path explanationDirPath = Paths.get("/Users/AdminDK/Desktop/TestExplanations/");
+		File explanationDir = new File(explanationDirPath.toString());
 		
 		File[] explanationFiles = explanationDir.listFiles(new FilenameFilter() {
 		    @Override
@@ -43,9 +49,9 @@ public class GenerateTrees {
 		for (int i = 0; i < explanationFiles.length; i++) {
 			
 			totalJustifications++;
-			String explanationFilename = explanationFiles[i].getAbsolutePath();		
-			InputStream fileInputStream = new FileInputStream(explanationFilename);
-			System.out.println(totalJustifications + " " + explanationFilename);
+			Path explanationFilePath = Paths.get(explanationFiles[i].getAbsolutePath());		
+			InputStream fileInputStream = new FileInputStream(explanationFilePath.toString());
+			System.out.println(totalJustifications + " " + explanationFilePath.toString());
 			Explanation<OWLAxiom> explanation = Explanation.load(fileInputStream);
 			
 			ExecutorService executor = Executors.newCachedThreadPool();
@@ -56,19 +62,38 @@ public class GenerateTrees {
 			try {
 				proofTrees = futureCall.get(10,TimeUnit.SECONDS);
 			} catch (TimeoutException e) {
-				System.out.println("TIMEOUT " +"Filename" + explanationFilename + " (Total: " + totalJustifications + ")");
+				System.out.println("TIMEOUT " +"Filename" + explanationFilePath.toString() + " (Total: " + totalJustifications + ")");
 			}
 			
 			if (proofTrees != null && proofTrees.size() > 0) {
 				totalTreesComputed++;
 			} else {
-				System.out.println("Could not compute Proof Tree." +"Filename" + explanationFilename + " (Total: " + totalJustifications + ")");
+				System.out.println("Could not compute Proof Tree." +" Filename " + explanationFilePath.toString() + " (Total: " + totalJustifications + ")");
+	//			copyFile(explanationFilePath, failedExplanationDirPath.resolve(explanationFilePath.getFileName()));
 			}				
 		}
 		double coverage = (totalTreesComputed * 100.0f)/totalJustifications;
 		
 		System.out.println("Coverage is: " + coverage);
 		
+	}
+	
+	
+	private static void copyFile(Path source, Path dest) throws IOException {
+	    InputStream is = null;
+	    OutputStream os = null;
+	    try {
+	        is = new FileInputStream(source.toAbsolutePath().toString());
+	        os = new FileOutputStream(dest.toAbsolutePath().toString());
+	        byte[] buffer = new byte[1024];
+	        int length;
+	        while ((length = is.read(buffer)) > 0) {
+	            os.write(buffer, 0, length);
+	        }
+	    } finally {
+	        is.close();
+	        os.close();
+	    }
 	}
 	
 }
