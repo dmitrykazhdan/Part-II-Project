fe5269fae18542e6d83da8f29dc1e793e25c5381
dmitrykazhdan <dmitry@kazhdan.net>
2018-03-13 23:16:42 +0000

    Add common expressions

--- src/CorpusAnalysis/TopBottomEntityCounter.java
+++ src/CorpusAnalysis/TopBottomEntityCounter.java
@@ -7,6 +7,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import org.semanticweb.owl.explanation.api.Explanation;
@@ -15,12 +19,25 @@ import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLClassExpression;
 
+import InferenceRules.RuleString;
+import OWLExpressionTemplates.AtomicCls;
+import OWLExpressionTemplates.CardExpGen;
+import OWLExpressionTemplates.ClsExpStr;
+import OWLExpressionTemplates.ExistsOrForAll;
+import OWLExpressionTemplates.ExpressionGroup;
+import OWLExpressionTemplates.InterUnion;
+import OWLExpressionTemplates.OWLAxiomStr;
+import OWLExpressionTemplates.SubClassStr;
+import RuleRestrictions.RuleRestriction;
+import RuleRestrictions.RuleRestrictions;
+import RuleRestrictions.SubSetRestriction;
+
 public class TopBottomEntityCounter {
 
 	
 	public static void main(String args[]) throws IOException {
 
-		Path justificationFolderPath = Paths.get("/Users/AdminDK/Desktop/Refined (06.03.2018)/NonTrivialComputedExplanations");
+		Path justificationFolderPath = Paths.get("/Users/AdminDK/Desktop/Refined (06.03.2018)/FailedExplanations");
 		File justificationFiles = new File(justificationFolderPath.toString());
 
 		File[] allFiles = justificationFiles.listFiles(new FilenameFilter() {
@@ -31,6 +48,7 @@ public class TopBottomEntityCounter {
 		});	
 		
 		int edgeEntityCount = 0;
+		List<OWLAxiom> expressions = new ArrayList<OWLAxiom>();
 		
 		for (File explanationFile : allFiles) {
 			
@@ -39,7 +57,7 @@ public class TopBottomEntityCounter {
 			Explanation<OWLAxiom> explanation = Explanation.load(fileInputStream);
 			Set<OWLAxiom> justification = explanation.getAxioms();
 			
-			boolean has = processJustification(justification);
+			boolean has = processJustification(justification, expressions);
 			
 			if (has) {
 				System.out.println(edgeEntityCount);
@@ -47,20 +65,26 @@ public class TopBottomEntityCounter {
 			}		
 			fileInputStream.close();
 		}		
-		System.out.println(edgeEntityCount);	
+		System.out.println(edgeEntityCount);
+		
+		Collections.shuffle(expressions);
+		for (OWLAxiom exp : expressions.subList(0, 200)) {
+			System.out.println(exp.toString());
+		}
 	}
 
 	
 	
-	private static boolean processJustification(Set<OWLAxiom> justification) {
+	private static boolean processJustification(Set<OWLAxiom> justification, List<OWLAxiom> expressionStrings) {
 		
 		for (OWLAxiom axiom : justification) {
 			
-			if (axiom.isLogicalAxiom()) {
+			if (axiom.isLogicalAxiom() && !matchesRule(axiom)) {
 				Set<OWLClass> expressions = axiom.getClassesInSignature();	
 				
 				for (OWLClass cls : expressions) {
-					if (cls.isOWLNothing() || cls.isOWLThing()) {
+					if (cls.isOWLThing()) {
+						expressionStrings.add(axiom);
 						return true;
 					}
 				}				
@@ -69,5 +93,39 @@ public class TopBottomEntityCounter {
 		return false;
 	}
 	
+	
+	private static boolean matchesRule(OWLAxiom axiom) {
+		
+		List<RuleString> rules = new ArrayList<RuleString>();
+		
+		ExpressionGroup tmpGroup1 = new ExpressionGroup("C1", new ClsExpStr[] { new AtomicCls("T")}, "Z" );
+		OWLAxiomStr premise1 = new OWLAxiomStr(AxiomType.DISJOINT_CLASSES, tmpGroup1);
+		RuleString r1 = new RuleString("X", "", premise1, premise1);
+		rules.add(r1);
+
+		premise1 = new SubClassStr(ExistsOrForAll.createObjSomeValFrom("Ro", "T"), "X");
+		r1 = new RuleString("X", "", premise1, premise1);
+		rules.add(r1);
+			
+		premise1 = new SubClassStr("X", CardExpGen.createObjExactCard("n", "Ro", "T"));
+		r1 = new RuleString("X", "", premise1, premise1);
+		rules.add(r1);
+		
+		premise1 = new SubClassStr("X", CardExpGen.createObjMaxCard("n", "Ro", "T"));
+		r1 = new RuleString("X", "", premise1, premise1);
+		rules.add(r1);
+		
+		premise1 = new SubClassStr("X", CardExpGen.createObjMinCard("n", "Ro", "T"));
+		r1 = new RuleString("X", "", premise1, premise1);
+		rules.add(r1);
+		
+		
+		for (RuleString r : rules) {
+			if (r.matchPremises(Arrays.asList(axiom))) {
+				return true;
+			}
+		}	
+		return false;
+	}
 	
 }
