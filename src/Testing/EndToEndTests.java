8cb25bf4a7653c8f4d99f802cf9512a9002d6a0c
dmitrykazhdan <dmitry@kazhdan.net>
2018-05-13 16:58:32 +0100

    Add end-to-end tests

--- src/Testing/EndToEndTests.java
+++ src/Testing/EndToEndTests.java
@@ -125,6 +125,117 @@ public class EndToEndTests {
 	}
 	
 	
+	@Test
+	public void test3() {
+		
+		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+		OWLDataFactory factory = manager.getOWLDataFactory();
+
+		OWLClass classX = factory.getOWLClass(IRI.create("urn:absolute:testingOntology#ClassX"));		
+		OWLClass classY = factory.getOWLClass(IRI.create("urn:absolute:testingOntology#ClassY"));
+		OWLClass classZ = factory.getOWLClass(IRI.create("urn:absolute:testingOntology#ClassZ"));
+
+		
+		OWLAxiom ax1 = factory.getOWLEquivalentClassesAxiom(classX, factory.getOWLObjectIntersectionOf(classY, classZ));
+		OWLAxiom ax2 = factory.getOWLDisjointClassesAxiom(classX, classY);
+		
+		OWLAxiom entailment = factory.getOWLSubClassOfAxiom(classX, factory.getOWLNothing())	;
+	
+		Set<OWLAxiom> justification = new HashSet<OWLAxiom>();
+		justification.add(ax1);	
+		justification.add(ax2);
+
+		Explanation<OWLAxiom> explanation = new Explanation<OWLAxiom>(entailment, justification);
+		List<ProofTree> proofTrees = ProofTreeGenerator.generateProofTrees(explanation);		
+		
+		removeDuplicateTrees(proofTrees);
+		assertTrue(proofTrees.size() == 1);
+		
+
+		ProofTree leaf1 = new ProofTree(ax1,null,null);
+		ProofTree leaf2 = new ProofTree(ax2,null,null);
+		
+		ProofTree i1 = new ProofTree(factory.getOWLSubClassOfAxiom(classX, classY), 
+									Arrays.asList(leaf1), GenerateRules.getRule("2.2"));
+
+		ProofTree ent = new ProofTree(factory.getOWLSubClassOfAxiom(classX, factory.getOWLNothing()), 
+				Arrays.asList(i1, leaf2), GenerateRules.getRule("15"));
+
+		
+		assertTrue(proofTrees.get(0).equals(ent));
+		
+	}
+	
+	
+	
+	@Test
+	public void test4() {
+		
+		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+		OWLDataFactory factory = manager.getOWLDataFactory();
+
+		OWLClass classX = factory.getOWLClass(IRI.create("urn:absolute:testingOntology#ClassX"));		
+		OWLClass classY1 = factory.getOWLClass(IRI.create("urn:absolute:testingOntology#ClassY1"));		
+		OWLClass classY2 = factory.getOWLClass(IRI.create("urn:absolute:testingOntology#ClassY2"));		
+		OWLClass classY3 = factory.getOWLClass(IRI.create("urn:absolute:testingOntology#ClassY3"));		
+		OWLClass classZ1 = factory.getOWLClass(IRI.create("urn:absolute:testingOntology#ClassZ1"));		
+		OWLClass classZ2 = factory.getOWLClass(IRI.create("urn:absolute:testingOntology#ClassZ2"));		
+		OWLClass classZ3 = factory.getOWLClass(IRI.create("urn:absolute:testingOntology#ClassZ3"));		
+		OWLClass classZ4 = factory.getOWLClass(IRI.create("urn:absolute:testingOntology#ClassZ4"));		
+		OWLClass classU = factory.getOWLClass(IRI.create("urn:absolute:testingOntology#ClassU"));		
+		OWLClass classV = factory.getOWLClass(IRI.create("urn:absolute:testingOntology#ClassV"));		
+
+		
+		OWLAxiom ax1 = factory.getOWLSubClassOfAxiom(classX, factory.getOWLObjectIntersectionOf(classY1, classY2, classY3));
+		OWLAxiom ax2 = factory.getOWLSubClassOfAxiom(factory.getOWLObjectIntersectionOf(classY1, classY2), factory.getOWLObjectIntersectionOf(classZ1, classZ2));
+		OWLAxiom ax3 = factory.getOWLSubClassOfAxiom(classU, classZ3);
+		OWLAxiom ax4 = factory.getOWLSubClassOfAxiom(classV, classZ3);
+		OWLAxiom ax5 = factory.getOWLSubClassOfAxiom(classX, factory.getOWLObjectUnionOf(classU, classV));
+
+		
+		OWLAxiom entailment = factory.getOWLSubClassOfAxiom(classX, factory.getOWLObjectIntersectionOf(classZ1, classZ3))	;
+	
+		Set<OWLAxiom> justification = new HashSet<OWLAxiom>();
+		justification.add(ax1);	
+		justification.add(ax2);
+		justification.add(ax3);
+		justification.add(ax4);
+		justification.add(ax5);
+		
+
+		Explanation<OWLAxiom> explanation = new Explanation<OWLAxiom>(entailment, justification);
+		List<ProofTree> proofTrees = ProofTreeGenerator.generateProofTrees(explanation);		
+		
+		removeDuplicateTrees(proofTrees);
+		assertTrue(proofTrees.size() == 1);
+		
+
+		ProofTree leaf1 = new ProofTree(ax1,null,null);
+		ProofTree leaf2 = new ProofTree(ax2,null,null);
+		ProofTree leaf3 = new ProofTree(ax4,null,null);
+		ProofTree leaf4 = new ProofTree(ax3,null,null);
+		ProofTree leaf5 = new ProofTree(ax5,null,null);
+		
+		ProofTree i1 = new ProofTree(factory.getOWLSubClassOfAxiom(classX, factory.getOWLObjectIntersectionOf(classY1, classY2)), 
+									Arrays.asList(leaf1), GenerateRules.getRule("3.1"));
+
+		ProofTree i2 = new ProofTree(factory.getOWLSubClassOfAxiom(factory.getOWLObjectIntersectionOf(classY1, classY2), classZ1), 
+				Arrays.asList(leaf2), GenerateRules.getRule("3.2"));
+		
+		ProofTree i3 = new ProofTree(factory.getOWLSubClassOfAxiom(classX, classZ1), 
+				Arrays.asList(i1, i2), GenerateRules.getRule("39"));
+
+		ProofTree i4 = new ProofTree(factory.getOWLSubClassOfAxiom(classX, classZ3), 
+				Arrays.asList(leaf3, leaf4, leaf5), GenerateRules.getRule("54"));
+
+		ProofTree ent = new ProofTree(factory.getOWLSubClassOfAxiom(classX, factory.getOWLObjectIntersectionOf(classZ1, classZ3)), 
+				Arrays.asList(i3, i4), GenerateRules.getRule("40"));
+
+		
+		assertTrue(proofTrees.get(0).equals(ent));
+		
+	}
+	
 	
 	
 	
